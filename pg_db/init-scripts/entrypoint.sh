5f91b6e0ca6558bbcf2214799cee5af799fb0011
JumbikTank <amolshevskii@edu.hse.ru>
2025-05-22 22:04:18 +0300

    edited entrypoint

--- pg_db/init-scripts/entrypoint.sh
+++ pg_db/init-scripts/entrypoint.sh
@@ -7,12 +7,5 @@ until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
   sleep 1
 done
 
-
-ORIGINAL_DIR=$(pwd)
-cd /usr/local/lib/python3.13/site-packages/flux_orm/
-alembic upgrade head
-cd "$ORIGINAL_DIR"
-echo "Alembic migrations completed."
-
 echo "Starting the main process..."
 exec "$@"
